42bf3ae033bbe4075edd6e252956f232431cb0cf
Yash Patel <yash1451999@gmail.com>
2024-08-14 18:11:27 -0400

    Heading test runs/ But not dynamic

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -3,9 +3,10 @@ import { render, screen } from "@testing-library/react";
 import Blog from "@/app/page";
 
 describe("Header", () => {
-  it("renders a heading", () => {
-    render(<Blog />);
-
+  it("renders a heading", async () => {
+    const Component = await Blog();
+    const screen = await render(Component);
+    // debug();
     const heading = screen.getByRole("heading", {
       name: "Blog",
     });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import { PostType } from "@/lib/types";
 import { getAllPosts } from "@/lib/action";
 
 const Blog = async () => {
-  const posts: PostType[] = (await getAllPosts()) as PostType[];
+  // const posts: PostType[] = (await getAllPosts()) as PostType[];
 
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-6">Blog</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {posts?.map((post) => (
+        {/* {posts?.map((post) => (
           <div key={post.id} className="w-full">
             <PostCard post={post} />
           </div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
